0b8f3179e036cf62c9be1309d51f3ac023dae71e
chonthicha-cheer <68030062@kmitl.ac.th>
2025-06-17 07:29:45 +0000

    add mult.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first nunber:"))
+num2 = int(input("Enter the second nunber:"))
+print(f"{10} * {5} = {50}")
+if -2 * 8 > -16:
+ print("The result is positive.")
+elif 20 * 0 < 0:
+ print("The result is negative.")
+else:
+  print("The result is positive and negative.")
