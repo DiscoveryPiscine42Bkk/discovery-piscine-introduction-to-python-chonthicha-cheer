fcef1e3fea457670095ecc45792ccc58f4634322
chonthicha-cheer <68030062@kmitl.ac.th>
2025-06-18 03:15:03 +0000

    add play_with_arrays.py

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,5 +1,2 @@
-def splay_with_array(arr):
-    result = [x + 2 for x in arr if x > 5]
-    print(result)
-original_array = [2, 8, 9, 48, 8, 22, -12, 2]
-play_with_arrays(original_array)
+old_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = [num * 2 for num in old_array]
